535539b9ba522deb58728815ea94a12a95a03dc0
Allison Nelson <nelsonam@users.noreply.github.com>
2015-05-14 12:24:43 -0700

    cleaned up the file

--- data/scripts/clean_urls.py
+++ data/scripts/clean_urls.py
@@ -10,8 +10,6 @@ with open('urls.csv', 'rb') as csvfile:
             if entry != '' and entry != 'http://www.amazon.com/Best-Sellers-Kindle-Store-eBooks/zgbs/digital-text/154606011/' and entry != 'title.href':
                 urls.append(entry)
 
-#for u in urls:
-#    print u
 for u in urls:
     split = u.split('/')
     for s in split:
