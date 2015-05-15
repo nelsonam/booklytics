612fb4ab5b9ac55189eb16666ee42561e8f70336
Allison Nelson <allison.nelson13@gmail.com>
2015-05-15 00:35:09 -0700

    added new output types, reading level, and ngrams

--- parse.py
+++ parse.py
@@ -7,6 +7,7 @@ import codecs
 from nltk.corpus import stopwords
 import nltk, re, pprint
 from nltk import word_tokenize
+from textstat.textstat import textstat
 
 def prepFile(filename):
     # prep file
@@ -15,6 +16,8 @@ def prepFile(filename):
         stop = stopwords.words('english')
         # the normal tokenized file
         tokens = word_tokenize(raw)
+        
+        nopunct = [w for w in tokens if w.isalpha()]
 
         # this stuff weeds out the stopwords/overly common words
         # to get a more meaningful freqdist
@@ -26,20 +29,42 @@ def prepFile(filename):
 
         ns_text = nltk.Text(nostops)
         text = nltk.Text(tokens)
-        return text,ns_text
+        np_text = nltk.Text(nopunct)
+        return raw, text,ns_text,np_text
 
 def describe(text):
-    
     # most common phrases that occur together
     text.collocations(50)
     # most common words
     fd = nltk.FreqDist(text)
     print fd.most_common(50)
 
-text, ns_text = prepFile(sys.argv[1])
+def ngrams(text, n):
+    # n-grams
+    print "n-grams"
+    ngrams = nltk.ngrams(text,n)
+    ngrams = nltk.FreqDist(ngrams)
+    ng = ngrams.most_common(25)
+    for n in ng:
+        print n
+    print "\n"
+    
+def reading_level(raw_text):
+    print "Flesch Reading Ease: ",textstat.flesch_reading_ease(raw_text)
+    print "Flesch-Kincaid Grade Level: ", textstat.flesch_kincaid_grade(raw_text)
+    print "Average Sentence Length: ", textstat.avg_sentence_length(raw_text)
+    print "Average Word Length: ", textstat.avg_letter_per_word(raw_text)
+
+raw, text, ns_text, np_text = prepFile(sys.argv[1])
 # this is the original text
 describe(text)
 print "\n"
 # this one has a lot of common words taken out
 describe(ns_text)
-
+print "\n"
+print "Reading Level"
+print "-------------"
+reading_level(raw)
+print "\n"
+# this needs non punct text to work best
+ngrams(np_text,5)
