f0745433eb335c5c887e762d6c20a4d05ccb5dd8
Allison Nelson <allison.nelson13@gmail.com>
2015-05-15 10:19:45 -0700

    took out some stopwords

--- cloud.py
+++ cloud.py
@@ -3,12 +3,13 @@ from wordcloud import WordCloud
 
 def genWordCloud(filename):
     text = open(filename).read()
-    words = WordCloud(width=1000, height=1000, font_path='/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf')
+    words = WordCloud(width=500, height=500, font_path='/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', stopwords=['s','t','said','m'])
     words.generate(text)
-    words.to_file(filename.split('.')[0] + '.png')
+    words.to_file(filename.split('.')[0] + '_500.png')
 
 if __name__ == '__main__':
 
     for i in range(1,15):
         genWordCloud('sources/'+str(i)+'.txt')
+
 
